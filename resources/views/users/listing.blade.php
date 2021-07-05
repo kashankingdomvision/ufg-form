79f21104d3b73d224cbee748bea20e6340444641
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-07-05 19:36:59 +0500

    pagination numbers fixed

--- resources/views/users/listing.blade.php
+++ resources/views/users/listing.blade.php
@@ -90,14 +90,7 @@
 
                             <div class="card-footer clearfix">
                                 <ul class="pagination pagination-sm m-0 float-right">
-                                    <li class="page-item"><a class="page-link" href="#">«</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">»</a></li>
-                                    {{-- {!! $users->render() !!} --}}
-
-                                    {{-- {{ dd( $users->links() ) }} --}}
+                                  {{ $users->links() }}
                                 </ul>
                             </div>
                         </div>
