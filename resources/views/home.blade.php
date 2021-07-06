a16654985ac794642f4b8afbc8b4e5005fe9d813
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-07-06 14:34:52 +0500

    some ui fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
+@section('title','Dashboard')
+
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center py-4">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,10 +4,11 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     
-    <a href="index3.html" class="brand-link">
-        <img src="{{ asset('img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+    <a href="{{ route('home') }}" class="brand-link">
+        <img src="{{ asset('img/logo.png') }}" height="50" width="230">
+        {{-- <img src="{{ asset('img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">AdminLTE 3</span> --}}
     </a>
 
     
