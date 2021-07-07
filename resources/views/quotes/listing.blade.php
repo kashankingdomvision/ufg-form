6ec20d8d1b8dba5ee726670180ab11b234e0de45
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-07-07 19:07:20 +0500

    fix ui quote listing

--- resources/views/quotes/listing.blade.php
+++ resources/views/quotes/listing.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.app')
 
-@section('content')
+@section('title','View Quote')
 
+@section('content')
 
     <div class="content-wrapper">
 
         <section class="content-header">
             <div class="container-fluid">
-                <div class="row mb-2">
+                <div class="row">
                     <div class="col-sm-6">
-                        <h4>View User</h4>
+                        <h4>View Quote</h4>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a>Home</a></li>
-                            <li class="breadcrumb-item active">User Management</li>
+                            <li class="breadcrumb-item active">Quote Management</li>
                         </ol>
                     </div>
                 </div>
