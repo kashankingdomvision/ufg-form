30eb7eddd7d7cfe12b33b4870b20065e041969a9
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-07-07 19:10:34 +0500

    booking migration

--- database/migrations/2021_07_01_095227_create_booking_details_table.php
+++ database/migrations/2021_07_01_095227_create_booking_details_table.php
@@ -39,7 +39,7 @@ class CreateBookingDetailsTable extends Migration
             $table->double('selling_price_bc')->nullable();
             $table->double('markup_amount_bc')->nullable();
             $table->enum('added_in_sage', [0, 1])->default(0);
-            $table->string('inovice');
+            $table->string('inovice')->nullable();;
             $table->timestamps();
             
             $table->foreign('quote_id')->references('id')->on('quotes')->onUpdate('cascade')->onDelete('cascade');
