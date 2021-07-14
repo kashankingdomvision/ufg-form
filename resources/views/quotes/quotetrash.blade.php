c6f26760d81eed4e1d85b0aa0180c4a3309910e8
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-07-14 15:57:04 +0500

    quote trash listing and restore the data

---
+++ resources/views/quotes/quotetrash.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+@section('title','View Quote')
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h4>View Quote Trash</h4>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a>Home</a></li>
+                            <li class="breadcrumb-item active">Quote Trash</li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-md-offset-3">
+                        @include('includes.flash_message')
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Quote List (Trash)</h3>
+                            </div>
+                            <div class="card-body p-0">
+                                <div class="table-responsive">
+                                    <table id="example1" class="table" >
+                                        <thead>
+                                          <tr>
+                                            <th></th>
+                                            <th>Zoho Ref #</th>
+                                            <th>Quote Ref #</th>
+                                            <th>Season</th>
+                                            <th>Brand</th>
+                                            <th>Booking Currency</th>
+                                            <th>Status</th>
+                                            <th>Booking Date</th>
+                                            <th>Created At</th>
+                                            <th>Action</th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                        @if($quotes && $quotes->count())
+                                            @foreach ($quotes as $key => $quote)
+                                                <tr>
+                                                <td>
+                                                    @if($quote->quote_count > 1)
+                                                    <button class="btn btn-sm addChild" id="show{{$quote->id}}" data-remove="#remove{{$quote->id}}" data-append="#appendChild{{$quote->id}}" data-ref="{{ $quote->ref_no }}" data-id="{{$quote->id}}">
+                                                        <span class="fa fa-plus"></span>
+                                                    </button>
+                                                    <button class="btn btn-sm removeChild" id="remove{{$quote->id}}" data-show="#show{{$quote->id}}" data-append="#appendChild{{$quote->id}}" data-ref="{{ $quote->ref_no }}" data-id="{{$quote->id}}" style="display:none;" >
+                                                        <span class="fa fa-minus"></span>
+                                                    </button>
+                                                    @endif
+                                                    </td>
+                                                    <td>{{ $quote->ref_no }}</td>
+                                                    <td>{{ $quote->quote_ref }}</td>
+                                                    <td>{{ $quote->getSeason->name }}</td>
+                                                    <td>{{ (isset($quote->getBrand->name))? $quote->getBrand->name: NULL }}</td>
+                                                    <td>{{ $quote->getBookingCurrency->code.' - '.$quote->getBookingCurrency->name }}</td>
+                                                    <td>{!! $quote->booking_formated_status !!}</td>
+                                                    <td>{{ $quote->formated_booking_date }}</td>
+                                                    <td>{{ $quote->formated_created_at }}</td>
+                                                    <td width="10%" class="d-flex" >
+                                                        <a onclick="return confirm('Are you sure want to restore {{ $quote->ref_no }}');" href="{{ route('quotes.restore', encrypt($quote->id)) }}" class="mr-2  btn btn-dark btn-xs" data-title="Restore" data-target="#Restore"><span class="fa fa-undo-alt"></span></a>
+                                                    </td>
+                                                    <tbody class="append" id="appendChild{{$quote->id}}" style="{{ $quote->quote_count > 1 ? 'background-color: #f9f9f9;' : null}}">
+                                                    </tbody>
+                                                </tr>
+                                            @endforeach
+                                        @else
+                                            <tr align="center"><td colspan="100%">No record found.</td></tr>
+                                        @endif
+                                        </tbody>
+                                      </table>
+                                </div>
+                            </div>
+                            <div class="card-footer clearfix">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
