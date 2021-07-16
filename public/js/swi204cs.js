09359495643f9e7f171f9d20f924afe1d62708f9
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-07-16 17:28:30 +0500

    reset supplier currency after clone

--- public/js/swi204cs.js
+++ public/js/swi204cs.js
@@ -19659,7 +19659,7 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).ready(function ($) {
     $('.product-id:last').html("<option selected value=\"\">Select Product</option>");
     $(".quote:last").attr('data-key', $('.quote').length - 1);
     $(".estimated-cost:last, .markup-amount:last, .markup-percentage:last, .selling-price:last, .profit-percentage:last, .estimated-cost-in-booking-currency:last, .selling-price-in-booking-currency:last, .markup-amount-in-booking-currency:last").val('0.00').attr('data-code', '');
-    $('.text-danger').html('');
+    $('.text-danger, .quote:last .supplier-currency-code').html('');
     $(".quote:last").prepend("<div class='row'><div class='col-sm-12'><button type='button' class='btn pull-right close'> x </button></div>");
     datepickerReset(1);
     reinitializedDynamicFeilds(); // datePickerSetDate();
