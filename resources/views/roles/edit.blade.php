b3118549d48216a352f94311574b44722dca6c3f
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-07-24 16:53:07 +0500

    Update edit.blade.php

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -1,67 +1,50 @@
 @extends('layouts.app')
-
 @section('title','Edit Role')
-
 @section('content')
-
-    <div class="content-wrapper">
-
-      <section class="content-header">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-6">
-                <h4>Edit Role</h4>
-              </div>
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a>Home</a></li>
-                  <li class="breadcrumb-item active">User Management</li>
-                </ol>
-            </div>
+<div class="content-wrapper">
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6">
+            <h4>Edit Role</h4>
           </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a>Home</a></li>
+              <li class="breadcrumb-item active">User Management</li>
+            </ol>
         </div>
-      </section>
-
-      <section class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="offset-md-2 col-md-8">
-
-              <div class="card card-primary">
-                <div class="card-header">
-                  <h3 class="card-title text-center">Role Form</h3>
-                </div>
-
-                <form method="POST" action="{{ route('roles.update',  encrypt($role->id)) }}">
-                  @csrf
-                  @method('put')
-
-                  <div class="card-body">
-
-                    <div class="form-group">
-                      <label>Name <span style="color:red">*</span></label>
-                      <input type="text" name="name" value="{{$role->name}}" class="form-control @error('name') is-invalid @enderror" placeholder="Role Name" required>
-
-                      @error('name')
-                        <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                      @enderror
-                    </div>
-
-                  </div>
-
-                  <div class="card-footer">
-                    <button type="submit" class="btn btn-primary float-right">Submit</button>
-                  </div>
-
-                </form>
-              </div>
-
-
-            </div>
-
-          </div>
-        </div>
-      </section>
-
+      </div>
     </div>
+  </section>
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="offset-md-2 col-md-8">
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title text-center">Role Form</h3>
+            </div>
+            <form method="POST" action="{{ route('roles.update',  encrypt($role->id)) }}">
+              @csrf
+              @method('put')
+              <div class="card-body">
+                <div class="form-group">
+                  <label>Name <span style="color:red">*</span></label>
+                  <input type="text" name="name" value="{{$role->name}}" class="form-control @error('name') is-invalid @enderror" placeholder="Role Name" required>
+                  @error('name')
+                    <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                  @enderror
+                </div>
+              </div>
+              <div class="card-footer">
+                <button type="submit" class="btn btn-primary float-right">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
 @endsection
