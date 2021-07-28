b4d986676c351ff2407692aaaf2d30463a3a2d81
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-07-28 15:51:51 +0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        abort(403);
+        // return view('home');
     }
     
     
