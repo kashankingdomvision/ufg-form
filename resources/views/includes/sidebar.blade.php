55c8954e48f9faff8561c7e7702971ac8d506772
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-08-06 11:00:29 +0500

    Update sidebar.blade.php

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -4,11 +4,8 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-    <a href="{{ route('home') }}" class="brand-link">
+    <a href="{{ route('dashboard.index') }}" class="brand-link">
         <img src="{{ asset('img/logo.png') }}" height="50" width="230">
-        {{-- <img src="{{ asset('img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span> --}}
     </a>
 
 
