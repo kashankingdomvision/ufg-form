1c60854d672c4940a1cf398283fbe371ce83bacd
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-08-17 17:14:48 +0500

    doc

--- resources/views/quote_documents/index.blade.php
+++ resources/views/quote_documents/index.blade.php
@@ -28,12 +28,17 @@
         
             <form method="POST" action="{{ route('quotes.document.pdf', $quote_id) }}" id="generate-pdf">
             @csrf
+            
+              
      
                 <div class="row">
                     <div class="col-md-12" >
                         <div id="document-editor__toolbar"></div>
                         <div id="toolbar-container"></div>
                         <div id="editor">
+                           
+                            <figure class="image ck-widget ck-widget_with-resizer image_resized ck-widget_selected" contenteditable="false" style="width:55.15%;"><img src="http://localhost/ufg-form/public/img/logo.png"><div class="ck ck-reset_all ck-widget__type-around"><div class="ck ck-widget__type-around__button ck-widget__type-around__button_before" title="Insert paragraph before block"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 8"><path d="M9.055.263v3.972h-6.77M1 4.216l2-2.038m-2 2 2 2.038"></path></svg></div><div class="ck ck-widget__type-around__button ck-widget__type-around__button_after" title="Insert paragraph after block"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 8"><path d="M9.055.263v3.972h-6.77M1 4.216l2-2.038m-2 2 2 2.038"></path></svg></div><div class="ck ck-widget__type-around__fake-caret"></div></div><div class="ck ck-reset_all ck-widget__resizer" style="height:288px;left:0px;top:0px;width:569px;"><div class="ck-widget__resizer__handle ck-widget__resizer__handle-top-left"></div><div class="ck-widget__resizer__handle ck-widget__resizer__handle-top-right"></div><div class="ck-widget__resizer__handle ck-widget__resizer__handle-bottom-right"></div><div class="ck-widget__resizer__handle ck-widget__resizer__handle-bottom-left"></div><div class="ck ck-size-view ck-orientation-bottom-right" style="display: none;">55.15%</div></div></figure>
+                            <hr />
                             @foreach ($documents as $key => $details)
                                 <h2><strong>{{ $details['day_date'] }}</strong></h2>
                                 <div>
