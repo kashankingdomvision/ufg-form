7817cd698a09caa32a55ac21ffbaf8f0efe92414
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-08-25 12:35:40 +0500

    Update show.blade.php

--- resources/views/quotes/show.blade.php
+++ resources/views/quotes/show.blade.php
@@ -43,7 +43,7 @@
 
         <div class="row">
           <div class="col-sm-6">
-              <h4>View Final Quote</h4>
+              <h4>View Quote > View(Booked Lead)</h4>
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
