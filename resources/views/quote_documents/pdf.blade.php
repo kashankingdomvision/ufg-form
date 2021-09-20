f3ff633d1607e198d724f28dc5460592620245a0
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-09-20 10:55:56 +0500

    Update pdf.blade.php

--- resources/views/quote_documents/pdf.blade.php
+++ resources/views/quote_documents/pdf.blade.php
@@ -124,24 +124,16 @@
             @endforeach
             </section>
             <section class="booking-condition">
-                <h2 class="text-center">Booking Condition</h2>
-                <hr>
-                <div class="cont-reapeat">
-                    <h4 class="mb-5">Lorem Ipsum</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <div class="cont-reapeat">
-                    <h4 class="mb-5">Lorem Ipsum</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <div class="cont-reapeat">
-                    <h4 class="mb-5">Lorem Ipsum</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <div class="cont-reapeat">
-                    <h4 class="mb-5">Lorem Ipsum</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
+                @if(count($storetexts) > 0)
+                    @foreach ($storetexts as $text)
+
+                        <h2 class="text-center">{{$text->page_title}}</h2>
+                        <hr />
+                        <div class="cont-reapeat">
+                            {!! $text->description !!}
+                        </div>
+                    @endforeach
+                @endif
             </section>
         </main>
     </body>
