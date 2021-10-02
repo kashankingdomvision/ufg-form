167adbe915a72f7f18a2cf341913cbe8c1339107
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-10-02 12:26:28 +0500

    user table seeder fix

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -19,7 +19,7 @@ class UserTableSeeder extends Seeder
                 'role_id'           =>  1,
                 'supervisor_id'     =>  NULL,
                 'currency_id'       =>  1,
-                'comission_id'      =>  NULL,
+                'commission_id'      =>  NULL,
                 'group_id'          =>  NULL,
                 'brand_id'          =>  1,
                 'holiday_type_id'   =>  1,
@@ -35,7 +35,7 @@ class UserTableSeeder extends Seeder
                 'role_id'           =>  5,
                 'supervisor_id'     =>  NULL,
                 'currency_id'       =>  2,
-                'comission_id'     =>  NULL,
+                'commission_id'     =>  NULL,
                 'group_id'          =>  NULL,
                 'brand_id'          =>  1,
                 'holiday_type_id'   =>  1,
@@ -51,7 +51,7 @@ class UserTableSeeder extends Seeder
                 'role_id'           =>  2,
                 'supervisor_id'     =>  2,
                 'currency_id'       =>  3,
-                'comission_id'     =>  NULL,
+                'commission_id'     =>  NULL,
                 'group_id'          =>  NULL,
                 'brand_id'          =>  1,
                 'holiday_type_id'   =>  1,
@@ -63,21 +63,21 @@ class UserTableSeeder extends Seeder
                 'updated_at' =>  now()
             ],
             
-            // [
-            //     'role_id'           =>  6,
-            //     'supervisor_id'     =>  null,
-            //     'currency_id'       =>  3,
-            //     // 'commission_id'     =>  NULL,
-            //     // 'group_id'          =>  NULL,
-            //     'brand_id'          =>  1,
-            //     'holiday_type_id'   =>  1,
-            //     'name'              =>  'louis Fonsi',
-            //     'email'             =>  'louisfonis@gmail.com',
-            //     'email_verified_at' =>  now(),
-            //     'password'          =>  Hash::make(12345678),
-            //     'created_at' =>  now(),
-            //     'updated_at' =>  now()
-            // ],
+            [
+                'role_id'           =>  6,
+                'supervisor_id'     =>  null,
+                'currency_id'       =>  3,
+                'commission_id'     =>  NULL,
+                'group_id'          =>  NULL,
+                'brand_id'          =>  1,
+                'holiday_type_id'   =>  1,
+                'name'              =>  'louis Fonsi',
+                'email'             =>  'louisfonis@gmail.com',
+                'email_verified_at' =>  now(),
+                'password'          =>  Hash::make(12345678),
+                'created_at' =>  now(),
+                'updated_at' =>  now()
+            ],
         ];
 
         User::insert($users);
