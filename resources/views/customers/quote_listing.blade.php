42711702d9259541c6a627d64960a18914523f1c
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-10-04 11:40:28 +0500

    customer quote listing fixed

--- resources/views/customers/quote_listing.blade.php
+++ resources/views/customers/quote_listing.blade.php
@@ -239,10 +239,10 @@
                                           </tr>
                                         </thead>
                                         <tbody>
-                                        @if($quotes && $quotes->count())
+                                            @if($quotes && $quotes->count())
                                             @foreach ($quotes as $key => $quote)
                                                 <tr class="{{ $quote->quote_count > 1 ? 'tbody-highlight' : ''}}">
-                                                
+
                                                     <td>
                                                         @if($quote->booking_status != 'booked')
                                                             <div class="icheck-primary">
@@ -271,12 +271,12 @@
                                                     @endif
 
                                                     @if($quote->booking_status == 'booked')
-                                                        <td> <a href="{{ route('bookings.show',encrypt($quote->getBooking->id)) }}">{{ $quote->quote_ref }}</a> </td>
+                                                        <td> <a href="{{ route('bookings.show', encrypt($quote->getBooking->id)) }}">{{ $quote->quote_ref }}</a> </td>
                                                     @endif
 
                                                     <td>{{ $quote->getSeason->name }}</td>
                                                     <td>{{ (isset($quote->getBrand->name))? $quote->getBrand->name: NULL }}</td>
-                                                    
+
                                                     <td>{{ $quote->getBookingCurrency->code.' - '.$quote->getBookingCurrency->name }}</td>
                                                     <td>{!! $quote->booking_formated_status !!}</td>
                                                     <td>{{ $quote->formated_booking_date }}</td>
@@ -291,24 +291,27 @@
                                                                 <button type="submit" onclick="return confirm('Are you sure you want to convert this Quotation to Booking?');" class="btn btn-outline-success btn-xs" data-title="" data-target="#" title="Convert to Booking"><span class="fa fa-check"></span></button>
                                                             </form>
                                                         @endif
-                                                        @if($quote->booking_status != 'booked')
+                                                        @if($quote->booking_status == 'quote' || $quote->booking_status == 'cancelled')
                                                             <a href="{{ route('quotes.final', encrypt($quote->id)) }}" title="View Quote" class="mr-2 btn btn-outline-info btn-xs" data-title="Final Quotation" data-target="#Final_Quotation">
                                                                 <span class="fa fa-eye"></span>
                                                             </a>
                                                         @endif
 
-                                                        @if ($quote->getBooking != NULL)
+                                                        @if($quote->booking_status == 'booked')
                                                             <a href="{{ route('bookings.show',encrypt($quote->getBooking->id)) }}" class="mr-2 btn btn-outline-success btn-xs" data-title="View Booking" title="View Booking" >
                                                                 <i class="fas fa-eye"></i>
                                                             </a>
                                                         @endif
-                                                      
-                                                        @if($quote->booking_status == 'quote' && $quote->deleted_at == null)
-                                                            <a onclick="return confirm('Are you sure want to cancel {{ $quote->ref_no }} ?');" href="{{ route('quotes.delete', encrypt($quote->id)) }}" class="mr-2  btn btn-outline-danger btn-xs" data-title="Cancel" title="Cancel" data-target="#Cancel"><span class="fa fa-times "></span></a>
-                                                            @elseif($quote->deleted_at != null)
-                                                            <a onclick="return confirm('Are you sure want to restore {{ $quote->ref_no }} ?');" href="{{ route('quotes.restore', encrypt($quote->id)) }}" class="mr-2  btn btn-success btn-xs" title="Restore" data-title="Restore" data-target="#Restore"><span class="fa fa-undo-alt"></span></a>
-                                                            
+
+                                                        @if($quote->booking_status == 'quote')
+                                                            <a onclick="return confirm('Are you sure you want to Cancel this Quote?');" href="{{ route('quotes.cancelled', encrypt($quote->id)) }}" class="mr-2 btn btn-outline-danger btn-xs" data-title="Cancel" title="Cancel Quote" data-target="#Cancel"><span class="fa fa-times "></span></a>
                                                         @endif
+
+                                                        @if($quote->booking_status == 'cancelled')
+                                                            <a onclick="return confirm('Are you sure you want to Restore this Quote?');" href="{{ route('quotes.restore', encrypt($quote->id)) }}" class="mr-2 btn btn-success btn-xs" title="Restore" data-title="Restore" data-target="#Restore"><span class="fa fa-undo-alt"></span></a>
+                                                        @endif
+
+
                                                         @if($quote->booking_status == 'booked')
                                                             <form class="mr-2 " method="POST" action="{{ route('quotes.archive.store', encrypt($quote->id)) }}">
                                                                 @csrf @method('patch')
@@ -328,14 +331,32 @@
                                                         {{-- <a href="{{ route('quotes.document', encrypt($quote->id)) }}" title="View" class="mr-2 btn btn-outline-info btn-xs" data-title="Document Quotation" data-target="#Document_Quotation">
                                                             <i class="fas fa-file"></i>
                                                         </a> --}}
+
+
                                                         @if($quote->booking_status == 'quote')
-                                                            <form class="mr-2 " method="POST" action="{{ route('quotes.clone', encrypt($quote->id)) }}">
+                                                            <form class="" method="POST" action="{{ route('quotes.clone', encrypt($quote->id)) }}">
                                                                 @csrf @method('patch')
                                                                 <button type="submit" title="Quote Clone"  onclick="return confirm('Are you sure you would like to clone this quote?');" class="mr-2 btn btn-outline-secondary btn-xs" data-title="Clone Quotation" data-target="#clone_quote">
                                                                     <i class="fa fa-clone"></i>
                                                                 </button>
                                                             </form>
                                                         @endif
+
+                                                        @if($quote->booking_status == 'quote')
+                                                            <form class="" method="POST" action="{{ route('quotes.export', encrypt($quote->id)) }}">
+                                                                @csrf
+                                                                <button type="submit" title="Export in Excel"  onclick="return confirm('Are you sure you would like to Export this Quote?');" class="mr-2 btn btn-outline-secondary btn-xs" data-title="Clone Quotation" data-target="#clone_quote">
+                                                                    <i class="fa fa-file-excel"></i>
+                                                                </button>
+                                                            </form>
+                                                        @endif
+
+                                                        @if($quote->booking_status == 'quote')
+                                                            <a href="{{ route('quotes.quote.documment',encrypt($quote->id)) }}" class="mr-2 btn btn-outline-success btn-xs" data-title="View Quote Document" title="View Quote Document" >
+                                                                <i class="fas fa-file"></i>
+                                                            </a>
+                                                        @endif
+
                                                     </td>
                                                     <tbody class="append {{ $quote->quote_count > 1 ? 'tbody-highlight' : ''}}" id="appendChild{{$quote->id}}">
                                                     </tbody>
