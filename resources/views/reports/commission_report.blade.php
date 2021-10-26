dbccb58289a9b4174d776b2d83fb486e3a0e6f79
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-10-26 16:01:08 +0500

    commission report filter fixed

--- resources/views/reports/commission_report.blade.php
+++ resources/views/reports/commission_report.blade.php
@@ -32,97 +32,119 @@
       </div>
     </div>
   </section>
-  
-  <x-page-filters :route="route('reports.commission.report')">
-    <div class="row">
 
+  <section class="content">
+    <form method="get" action="{{ route('reports.commission.report') }}">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                  <div class="card card-default ">
+                    <button type="button" class="btn btn-tool m-0 text-dark " data-card-widget="collapse">
+                      <div class="card-header">
+                        <h3 class="card-title"><b> <i class="fas fa-filter" aria-hidden="true"></i>  Filters &nbsp;<i class="fa fa-angle-down"></i></b></h3>
+                      </div>
+                    </button>
 
-        <div class="col-md-3">
-            <div class="form-group">
-                <label>Commision </label>
-                <select class="form-control select2single" name="commission_id">
-                    <option value="">Select Commision</option>
-                    @foreach ($commissions as $commission)
-                        <option value="{{ $commission->id }}" {{ (old('commission_id') == $commission->id)? 'selected': ((request()->get('commission_id') == $commission->id)? 'selected' : null) }} >{{ $commission->name }}</option>
-                    @endforeach
-                </select>
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Commision </label>
+                                <select class="form-control select2single" name="commission_id">
+                                    <option value="">Select Commision</option>
+                                    @foreach ($commissions as $commission)
+                                        <option value="{{ $commission->id }}" {{ (old('commission_id') == $commission->id)? 'selected': ((request()->get('commission_id') == $commission->id)? 'selected' : null) }} >{{ $commission->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Commission Group </label>
+                                <select class="form-control select2single" name="commission_group_id">
+                                    <option value="">Select Commission Group</option>
+                                    @foreach ($commission_groups as $commission_group)
+                                        <option value="{{ $commission_group->id }}" {{ (old('commission_group_id') == $commission_group->id)? 'selected': ((request()->get('commission_group_id') == $commission_group->id)? 'selected' : null) }} >{{ $commission_group->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Booking Currency</label>
+                                <select class="form-control select2-multiple "  data-placeholder="Select Booking Currency" multiple name="booking_currency[]">
+                                    @foreach ($currencies as $curren)
+                                        <option value="{{ $curren->id }}" data-image="data:image/png;base64, {{$curren->flag}}" {{ (old('booking_currency') == $curren->id)? 'selected': ( (!empty(request()->get('booking_currency')))? (((in_array($curren->id, request()->get('booking_currency'))))? 'selected' : null) : '') }}> &nbsp; {{$curren->code}} - {{$curren->name}} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Sales Person</label>
+                                <select class="form-control select2single" name="sale_person_id">
+                                    <option value="">Select Sales Person</option>
+                                    @foreach ($users as $user)
+                                        <option value="{{ $user->id }}"  {{ (old('sale_person_id') == $user->id) ? 'selected': ((request()->get('sale_person_id') == $user->id) ? 'selected' : null) }}>{{ $user->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Brand</label>
+                                <select class="form-control select2single getBrandtoHoliday" name="brand_id">
+                                    <option value="">Select Brand</option>
+                                    @foreach ($brands as $brand)
+                                        <option value="{{ $brand->id }}" {{ (old('brand_id') == $brand->id) ? 'selected': ((request()->get('brand_id') == $brand->id) ? 'selected' : null) }}>{{ $brand->name }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                
+                        <div class="col-md-3">
+                            <div class="form-group">
+                              <label>Type Of Holiday </label>
+                              <select name="holiday_type_id" id="holiday_type_id" class="form-control select2single appendHolidayType holiday-type-id">
+                                <option value="">Select Type Of Holiday</option>
+                              </select>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Booking Season</label>
+                                <select class="form-control select2single" name="season_id">
+                                    <option value="" selected >Select Booking Season</option>
+                                    @foreach ($booking_seasons as $seasons)
+                                        <option value="{{ $seasons->id }}" {{ (old('season_id') == $seasons->id)? 'selected': ((request()->get('season_id') == $seasons->id)? 'selected' : null) }}>{{ $seasons->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                      </div>
+
+                      <div class="row mt-1">
+                        <div class="col-md-12">
+                          <div class="d-flex justify-content-end">
+                            <button type="submit" class="btn btn-outline-success btn-md mr-2" style="width: 10rem;">Filter</button>
+                            <a href="{{ route('reports.commission.report') }}" class="btn btn-outline-dark">Reset<span class="fa fa-repeats"></span></a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
             </div>
         </div>
-
-        <div class="col-md-3">
-            <div class="form-group">
-                <label>Commission Group </label>
-                <select class="form-control select2single" name="commission_group_id">
-                    <option value="">Select Commission Group</option>
-                    @foreach ($commission_groups as $commission_group)
-                        <option value="{{ $commission_group->id }}" {{ (old('commission_group_id') == $commission_group->id)? 'selected': ((request()->get('commission_group_id') == $commission_group->id)? 'selected' : null) }} >{{ $commission_group->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-
-        <div class="col-md-3">
-            <div class="form-group">
-                <label>Booking Currency</label>
-                <select class="form-control select2-multiple "  data-placeholder="Select Booking Currency" multiple name="booking_currency[]">
-                    @foreach ($currencies as $curren)
-                        <option value="{{ $curren->id }}" data-image="data:image/png;base64, {{$curren->flag}}" {{ (old('booking_currency') == $curren->id)? 'selected': ( (!empty(request()->get('booking_currency')))? (((in_array($curren->id, request()->get('booking_currency'))))? 'selected' : null) : '') }}> &nbsp; {{$curren->code}} - {{$curren->name}} </option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-
-        <div class="col-md-3">
-            <div class="form-group">
-                <label>Sales Person</label>
-                <select class="form-control select2single" name="sale_person_id">
-                    <option value="">Select Sales Person</option>
-                    @foreach ($users as $user)
-                        <option value="{{ $user->id }}"  {{ (old('sale_person_id') == $user->id) ? 'selected': ((request()->get('sale_person_id') == $user->id) ? 'selected' : null) }}>{{ $user->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-
-        <div class="col-md-3">
-            <div class="form-group">
-                <label>Brand</label>
-                <select class="form-control select2single getBrandtoHoliday" name="brand_id">
-                    <option value="">Select Brand</option>
-                    @foreach ($brands as $brand)
-                        <option value="{{ $brand->id }}" {{ (old('brand_id') == $brand->id) ? 'selected': ((request()->get('brand_id') == $brand->id) ? 'selected' : null) }}>{{ $brand->name }} </option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-
-
-        <div class="col-md-3">
-            <div class="form-group">
-              <label>Type Of Holiday </label>
-              <select name="holiday_type_id" id="holiday_type_id" class="form-control select2single appendHolidayType holiday-type-id">
-                <option value="">Select Type Of Holiday</option>
-              </select>
-            </div>
-        </div>
-
-        
-        <div class="col-md-3">
-            <div class="form-group">
-                <label>Booking Season</label>
-                <select class="form-control select2single" name="season_id">
-                    <option value="" selected >Select Booking Season</option>
-                    @foreach ($booking_seasons as $seasons)
-                        <option value="{{ $seasons->id }}" {{ (old('season_id') == $seasons->id)? 'selected': ((request()->get('season_id') == $seasons->id)? 'selected' : null) }}>{{ $seasons->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-
-
-
-    </div>
-  </x-page-filters>
+    </form>
+</section>
+ 
 
   <section class="content p-2">
     <div class="container-fluid">
