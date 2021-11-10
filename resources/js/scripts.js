69afe7914006e247304165d84bbd58711c042e80
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-11-10 13:19:26 +0500

    compile asset for dev

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -13,14 +13,13 @@ import 'jquery-ui/ui/widgets/sortable.js';
 // import { Alert } from 'bootstrap';
 // import { isArguments } from 'lodash-es';
 
-var BASEURL          = `${window.location.origin}/ufg-form/public/json/`;
-var REDIRECT_BASEURL = `${window.location.origin}/ufg-form/public/`;
-// var BASEURL          = `${window.location.origin}/php/ufg-form/public/json/`;
-// var REDIRECT_BASEURL = `${window.location.origin}/php/ufg-form/public/`;
+// var BASEURL          = `${window.location.origin}/ufg-form/public/json/`;
+// var REDIRECT_BASEURL = `${window.location.origin}/ufg-form/public/`;
+var BASEURL          = `${window.location.origin}/php/ufg-form/public/json/`;
+var REDIRECT_BASEURL = `${window.location.origin}/php/ufg-form/public/`;
  
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios = require('axios');
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 var CSRFTOKEN = $('#csrf-token').attr('content');
 
