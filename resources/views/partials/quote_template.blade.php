a6b5a28341d253887e05c91c0e93268f26d43469
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-11-22 19:29:24 +0500

    correct place holder

--- resources/views/partials/quote_template.blade.php
+++ resources/views/partials/quote_template.blade.php
@@ -42,7 +42,7 @@
             <div class="col-sm-2">
               <div class="form-group">
                 <label>Date of Service <span style="color:red">*</span></label>
-                <input type="text" value="{{ $q_detail->date_of_service }}" name="quote[{{ $key }}][date_of_service]" data-name="date_of_service" id="quote_{{ $key }}_date_of_service" class="form-control date-of-service datepicker checkDates bookingDateOfService"  placeholder="Date of Service" autocomplete="off">
+                <input type="text" value="{{ $q_detail->date_of_service }}" name="quote[{{ $key }}][date_of_service]" data-name="date_of_service" id="quote_{{ $key }}_date_of_service" class="form-control date-of-service datepicker checkDates bookingDateOfService"  placeholder="DD/MM/YYYY" autocomplete="off">
                 <span class="text-danger" role="alert"></span>
               </div>
             </div>
