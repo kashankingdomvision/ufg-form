f993abc4312424d859a705f1f1a3133278827df4
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-11-30 19:41:08 +0500

    locations seeder, migration

--- database/seeds/InitialTableSeeder.php
+++ database/seeds/InitialTableSeeder.php
@@ -18,6 +18,9 @@ class InitialTableSeeder extends Seeder
         $sql = file_get_contents(database_path() . '/seeds/sql_dump/countries.sql');
         DB::unprepared($sql);
 
+        $sql = file_get_contents(database_path() . '/seeds/sql_dump/locations.sql');
+        DB::unprepared($sql);
+
         $sql = file_get_contents(database_path() . '/seeds/sql_dump/hotels.sql');
         DB::statement($sql);
 
