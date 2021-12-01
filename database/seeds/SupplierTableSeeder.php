e6ad52fea5662674a03b8412f23ae695d5491083
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-12-01 20:45:51 +0500

    supplier & supplier categories table import

--- database/seeds/SupplierTableSeeder.php
+++ database/seeds/SupplierTableSeeder.php
@@ -14,62 +14,68 @@ class SupplierTableSeeder extends Seeder
      */
     public function run()
     {
-        $data = [
-            [
-                'currency_id'   => 1,
-                'country_id'    => 56,
-                'location_id'   => 1,
-                'name'  => 'Jaun Elia',
-                'email' => 'jaun@gmail.com',
-                'phone' => '03024214334',
-                'categories' => [2,4,5],
-                'products' => 1,
-            ],
+        $sql = file_get_contents(database_path() . '/seeds/sql_dump/suppliers.sql');
+        DB::unprepared($sql);
+
+        $sql = file_get_contents(database_path() . '/seeds/sql_dump/supplier_categories.sql');
+        DB::unprepared($sql);
+
+        // $data = [
+        //     [
+        //         'currency_id'   => 1,
+        //         'country_id'    => 56,
+        //         'location_id'   => 1,
+        //         'name'  => 'Jaun Elia',
+        //         'email' => 'jaun@gmail.com',
+        //         'phone' => '03024214334',
+        //         'categories' => [2,4,5],
+        //         'products' => 1,
+        //     ],
             
-            [
-                'currency_id'   => 3,
-                'country_id'    => 56,
-                'location_id'   => 1,
-                'name'  => 'Kevin Levin',
-                'email' => 'kevinlevin@yahoo.co.uk',
-                'phone' => '+92034973095',
-                'products' => 2,
-                'categories' => [3,1,5],
-            ],
+        //     [
+        //         'currency_id'   => 3,
+        //         'country_id'    => 56,
+        //         'location_id'   => 1,
+        //         'name'  => 'Kevin Levin',
+        //         'email' => 'kevinlevin@yahoo.co.uk',
+        //         'phone' => '+92034973095',
+        //         'products' => 2,
+        //         'categories' => [3,1,5],
+        //     ],
             
-            [
-                'currency_id'   => 2,
-                'country_id'    => 56,
-                'location_id'   => 1,
-                'name'  => 'Peter Parker',
-                'email' => 'parker@yahoo.co.uk',
-                'phone' => '89798693322',
-                'products' => 2,
-                'categories' => [3,1,5],
-            ],
-        ];
+        //     [
+        //         'currency_id'   => 2,
+        //         'country_id'    => 56,
+        //         'location_id'   => 1,
+        //         'name'  => 'Peter Parker',
+        //         'email' => 'parker@yahoo.co.uk',
+        //         'phone' => '89798693322',
+        //         'products' => 2,
+        //         'categories' => [3,1,5],
+        //     ],
+        // ];
         
-        foreach ($data as $supp) {
-            $supplier = Supplier::create([
-                'currency_id'   => $supp['currency_id'],
-                'country_id'    => $supp['country_id'],
-                'location_id'   => $supp['location_id'],
-                'name'          => $supp['name'],       
-                'email'         => $supp['email'],      
-                'phone'         => $supp['phone'],      
-            ]);
-            foreach ($supp['categories'] as $cate) {
-                SupplierCategory::create([
-                    'supplier_id' => $supplier->id,
-                    'category_id' => $cate,
-                ]);
-            }
+        // foreach ($data as $supp) {
+        //     $supplier = Supplier::create([
+        //         'currency_id'   => $supp['currency_id'],
+        //         'country_id'    => $supp['country_id'],
+        //         'location_id'   => $supp['location_id'],
+        //         'name'          => $supp['name'],       
+        //         'email'         => $supp['email'],      
+        //         'phone'         => $supp['phone'],      
+        //     ]);
+        //     foreach ($supp['categories'] as $cate) {
+        //         SupplierCategory::create([
+        //             'supplier_id' => $supplier->id,
+        //             'category_id' => $cate,
+        //         ]);
+        //     }
             
-            SupplierProduct::create([
-                'supplier_id' => $supplier->id,
-                'product_id'  =>  $supp['products'],
-            ]);
-        }
+        //     SupplierProduct::create([
+        //         'supplier_id' => $supplier->id,
+        //         'product_id'  =>  $supp['products'],
+        //     ]);
+        // }
         
         
     }
