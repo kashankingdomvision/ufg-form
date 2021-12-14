d8ffb784b6076bc33d7cb7a30eeeaadd744e8d09
kashankingdomvision <kashan.kingdomvision@gmail.com>
2021-12-14 11:52:10 +0500

    set_end_date_of_service of transfer

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -15,12 +15,14 @@ class CategoryTableSeeder extends Seeder
     {
         $categories = ['Transfer', 'Accommodation', 'Tours', 'Cruise', 'Taxes', 'Service Excursion','Ferry / Catamaran','Train', 'Flights','Misc.','Airline'];
         foreach ($categories as $key => $category) {
-            Category::create([
+            $category = Category::create([
 
                 'name'       => $category, 
                 'slug'       => Str::slug($category),
                 'sort_order' => $key+1,
             ]);
         }
+
+        Category::where('id', 1)->update([ 'set_end_date_of_service' => '1' ]);
     }
 }
