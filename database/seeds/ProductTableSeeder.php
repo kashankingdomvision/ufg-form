bd3ad0732b39f191b44914e2e52aac2ea72a193f
kashankingdomvision <kashan.kingdomvision@gmail.com>
2022-01-26 15:04:39 +0500

    add cruise product

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -18,8 +18,8 @@ class ProductTableSeeder extends Seeder
         $sql = file_get_contents(database_path() . '/seeds/sql_dump/products.sql');
         DB::unprepared($sql);
 
-        // create product for transer (category)
-        $products = [ 
+        /* dummy product for transfer category */ 
+        $transfer_products = [ 
             [
                 'code'             => 'PA',
                 'name'             => 'PIA Airline',
@@ -38,7 +38,61 @@ class ProductTableSeeder extends Seeder
             ]
         ];
 
-        Product::insert($products);
+        /* dummy product for cruise category */ 
+        $cruise_products = [ 
+            [
+                'code'             => 'SD-Cruise',
+                'name'             => 'Signature Split to Dubrovnik Cruise',
+                'category_id'      => 4,
+                'country_id'       => NULL,
+                'location_id'      => 1,
+                'currency_id'      => NULL,
+                'booking_type_id'  => 1,
+                'duration'         => NULL,
+                'price'            => NULL,
+                'description'      => NULL,
+                'inclusions'       => NULL,
+                'packing_list'     => NULL,
+                'created_at'       => now(),
+                'updated_at'       => now(),
+            ],
+            [
+                'code'             => 'DS-Cruise',
+                'name'             => 'Deluxe Dubrovnik to Split Cruise',
+                'category_id'      => 4,
+                'country_id'       => NULL,
+                'location_id'      => 1,
+                'currency_id'      => NULL,
+                'booking_type_id'  => 1,
+                'duration'         => NULL,
+                'price'            => NULL,
+                'description'      => NULL,
+                'inclusions'       => NULL,
+                'packing_list'     => NULL,
+                'created_at'       => now(),
+                'updated_at'       => now(),
+            ],
+            [
+                'code'             => 'NDD-Cruise',
+                'name'             => 'Northern Dalmatia Deluxe Cruise',
+                'category_id'      => 4,
+                'country_id'       => NULL,
+                'location_id'      => 1,
+                'currency_id'      => NULL,
+                'booking_type_id'  => 1,
+                'duration'         => NULL,
+                'price'            => NULL,
+                'description'      => NULL,
+                'inclusions'       => NULL,
+                'packing_list'     => NULL,
+                'created_at'       => now(),
+                'updated_at'       => now(),
+            ],
+        ];
+        
+        Product::insert($cruise_products);
+        Product::insert($transfer_products);
+
     }
    
 }
