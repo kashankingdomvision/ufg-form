8d391b2b0ee1c5a7fe6072310f43849d84318101
kashankingdomvision <kashan.kingdomvision@gmail.com>
2022-01-26 15:07:31 +0500

    hide product/ category details feilds

--- resources/views/quotes/create.blade.php
+++ resources/views/quotes/create.blade.php
@@ -551,7 +551,7 @@
                               </div>
                             </div>
 
-                            <div class="col-sm-2">
+                            <div class="col-sm-2 d-none">
                               <div class="form-group">
                                 <label>Category Details</label>
                                 <input type="text" name="quote[0][category_details]" value="" id="quote_0_category_details" class="form-control category-details">
@@ -596,7 +596,7 @@
                               </div>
                             </div>
 
-                            <div class="col-sm-2">
+                            <div class="col-sm-2 d-none">
                               <div class="form-group">
                                 <label>Product Details</label>
                                 <input type="text" name="quote[0][product_details]" value="" id="quote_0_product_details" class="form-control product-details">
--- resources/views/quotes/edit.blade.php
+++ resources/views/quotes/edit.blade.php
@@ -686,7 +686,7 @@
                                 </div>
                               </div>
 
-                              <div class="col-sm-2 ">
+                              <div class="col-sm-2 d-none">
                                 <div class="form-group">
                                   <label>Category Details</label>
                                   {{-- <input type="text" name="quote[{{ $key }}][category_details]" value="@if(isset($q_detail->getCategory->quote) && isset($q_detail->getCategory->quote) == 1)@if(empty($q_detail->category_details) || is_null($q_detail->category_details)){{ $q_detail->getCategory->feilds }}@else{{$q_detail->category_details}}@endif @endif" id="quote_{{ $key }}_category_details" class="form-control category-details"> --}}
@@ -763,7 +763,7 @@
                                 </div>
                               </div>
 
-                              <div class="col-sm-2">
+                              <div class="col-sm-2 d-none">
                                 <div class="form-group">
                                   <label>Product Details</label>
                                   <input type="text" name="quote[{{ $key }}][product_details]" value="{{ $q_detail->product_details }}" id="quote_{{ $key }}_product_details" class="form-control product-details">
