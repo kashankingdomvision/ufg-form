5479bea8eecaf136493f431d8c874dd07a3a8dd4
kashankingdomvision <kashan.kingdomvision@gmail.com>
2022-02-03 11:57:33 +0500

    indent add ui effect & border & icon

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light shadow-sm">
 
   <ul class="navbar-nav">
     <li class="nav-item">
@@ -6,35 +6,32 @@
     </li>
   </ul>
 
-  <ul class="navbar-nav ml-auto">
-
-    <li class="nav-item dropdown">
-      <a class="nav-link" data-toggle="dropdown" href="#">
-        {{ (Auth::user()->name) }}
-        &nbsp;&nbsp;<i class="fa fa-user" aria-hidden="true"></i>
+  <ul class="navbar-nav ml-auto mr-1">
+    <li class="nav-item dropdown border rounded">
+      <a class="nav-link align-middle" data-toggle="dropdown" href="#">
+        <span>{{ (Auth::user()->name) }}</span> &nbsp;
+        <i class="fa fa-caret-down" aria-hidden="true"></i>
       </a>
 
-
-      <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+      <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right shadow-sm">
         <div class="dropdown-divider"></div>
-          <a href="{{ route('users.edit', [encrypt(Auth::id()), 'profile']) }}" class="dropdown-item">
-            <i class="fas fa-user-edit mr-2"></i>Edit Profile
-          </a>   
-          <div class="dropdown-divider"></div>
-          <form id="frm-logout" action="{{ route('logout') }}" method="POST" >
-            @csrf
-            <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
-              <i class="fas fa-sign-out-alt mr-2"></i>Logout
-            </a>    
-          </form>
-
-          {{-- <a href="#" >
-            <i class="fas fa-sign-out-alt mr-2"></i>Logout --}}
-            {{-- <span class="float-right text-muted text-sm">2 days</span> --}}
-          {{-- </a> --}}
-
+        <a href="{{ route('users.edit', [encrypt(Auth::id()), 'profile']) }}" class="dropdown-item">
+          <i class="fas fa-user-edit mr-2"></i>Edit Profile
+        </a>   
+        <div class="dropdown-divider"></div>
+        <form id="frm-logout" action="{{ route('logout') }}" method="POST" >
+          @csrf
+          <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
+            <i class="fas fa-sign-out-alt mr-2"></i>Logout
+          </a>    
+        </form>
       </div>
     </li>
-
   </ul>
+  
 </nav>
+
+{{-- <a href="#" >
+  <i class="fas fa-sign-out-alt mr-2"></i>Logout --}}
+  {{-- <span class="float-right text-muted text-sm">2 days</span> --}}
+{{-- </a> --}}
