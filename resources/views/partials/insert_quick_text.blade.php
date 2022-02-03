691757a0ad6097a21960b1235a0dd82095c4388a
kashankingdomvision <kashan.kingdomvision@gmail.com>
2022-02-03 14:31:44 +0500

    set sizing & othe

--- resources/views/partials/insert_quick_text.blade.php
+++ resources/views/partials/insert_quick_text.blade.php
@@ -1,7 +1,6 @@
 <div class="modal fade insert-quick-text-modal">
-  <div class="modal-dialog modal-xl">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      @csrf 
       <div class="modal-header">
         <h4 class="modal-title">Insert Quick Comment</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -10,28 +9,22 @@
       </div>
 
       <div class="modal-body">
-        <form>
-          <div class="row" style="max-height: 500px; overflow-y: auto;">
-
-            @if($preset_comments && $preset_comments->count())
-              @foreach ($preset_comments as $key => $preset_comment)
-                <div class="col-sm-4">
-                  <div class="form-group">
-                    <div>
-                      <label class="radio-inline mr-1">
-                        <input type="radio" name="comment" value="{{ $preset_comment->comment }}" class="quick-comment">
-                        <span>&nbsp; {{ $preset_comment->comment }} </span>
-                      </label>
-                    </div>
+        <div class="row" style="max-height: 500px; overflow-y: auto;">
+          @if($preset_comments && $preset_comments->count())
+            @foreach ($preset_comments as $key => $preset_comment)
+              <div class="col-md-4">
+                <div class="form-group">
+                  <div>
+                    <input type="radio" name="comment" value="{{ $preset_comment->comment }}" id="quick_comment_{{$key}}" class="quick-comment">
+                    <label class="radio-inline mr-1" for="quick_comment_{{$key}}">{{ $preset_comment->comment }}</label>
                   </div>
                 </div>
-              @endforeach
-            @else
-              No record found.
-            @endif
-              
-          </div>
-        </form>
+              </div>
+            @endforeach
+          @else
+            No record found.
+          @endif
+        </div>
       </div>
 
       <div class="modal-footer">
