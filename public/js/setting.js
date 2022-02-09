1cd5a9d1fbcfb0ce8b72517e99e4b523459b152a
kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>
2022-02-09 12:17:50 +0000

    add quote_management in laravel mix

--- public/js/setting.js
+++ public/js/setting.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 5);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -1087,7 +1087,7 @@ $(document).ready(function () {
 
 /***/ }),
 
-/***/ 4:
+/***/ 5:
 /*!***************************************!*\
   !*** multi ./resources/js/setting.js ***!
   \***************************************/
--- public/js/user_management.js
+++ public/js/user_management.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -253,7 +253,7 @@ $(document).ready(function () {
 
 /***/ }),
 
-/***/ 3:
+/***/ 4:
 /*!***********************************************!*\
   !*** multi ./resources/js/user_management.js ***!
   \***********************************************/
