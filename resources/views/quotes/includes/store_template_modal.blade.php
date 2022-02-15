d1a15693fa55b096bfb415144a349d3823caa0ac
kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>
2022-02-15 12:36:41 +0000

    correct radio button in template ui

--- resources/views/quotes/includes/store_template_modal.blade.php
+++ resources/views/quotes/includes/store_template_modal.blade.php
@@ -17,27 +17,29 @@
 
           <div class="form-group">
             <label>Privacy Status <span style="color:red">*</span></label>
-            <div>
-              <label class="radio-inline mr-1">
-                <input type="radio" name="privacy_status" value="1" class="privacy-status" checked>
-                <span>&nbsp;Public  </span>
-              </label>
-              <label class="radio-inline mr-1">
-                <input type="radio" name="privacy_status" value="0" class="privacy-status">
-                <span>&nbsp;Private </span>
-              </label>
+            <div class="d-flex flex-row">
+
+              <div class="custom-control custom-radio mr-1">
+                <input type="radio" name="privacy_status" id="privacy_status_public" value="1" class="privacy-status custom-control-input custom-control-input-success custom-control-input-outline" checked>
+                <label class="custom-control-label" for="privacy_status_public">Public</label>
+              </div>
+
+              <div class="custom-control custom-radio mr-1">
+                <input type="radio" name="privacy_status" id="privacy_status_private" value="0" class="privacy-status custom-control-input custom-control-input-success custom-control-input-outline">
+                <label class="custom-control-label" for="privacy_status_private">Private</label>
+              </div>
             </div>
           </div>
-          
+
         </div>
 
         <div class="modal-footer justify-content-right">
-          <button class="btn btn-primary" type="button" id="submit_template">
+          <button class="btn btn-primary btn-sm" type="button" id="submit_template">
             <span class="mr-2 " role="status" aria-hidden="true"></span>
             Save Template
           </button>
 
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Close</button>
         </div>
 
       </div>
