8d5a882ba8fc36a700f5bd7f4697bd5b8d3e5e6a
kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>
2022-03-03 16:54:07 +0500

    Start & End DoS Same Automatically For "Train"

--- database/seeds/sql_dump/categories.sql
+++ database/seeds/sql_dump/categories.sql
@@ -21,6 +21,9 @@ UPDATE `categories` SET `label_of_time` = 'Departure Time' WHERE `categories`.`i
 UPDATE `categories` SET `second_tf` = '1' WHERE `categories`.`id` = 6;
 UPDATE `categories` SET `second_label_of_time` = 'Arrival Time' WHERE `categories`.`id` = 6;
 
+--  Train
+UPDATE `categories` SET `set_end_date_of_service` = '1' WHERE `categories`.`id` = 7;
+
 -- Flights
 UPDATE `categories` SET `feilds` = '[{\"type\":\"autocomplete\",\"label\":\"Departure Airport\",\"className\":\"form-control\",\"name\":\"autocomplete-1643896471930-0\",\"requireValidOption\":false,\"data\":\"airport_codes\",\"values\":[]},{\"type\":\"autocomplete\",\"label\":\"Arrival Airport\",\"className\":\"form-control\",\"name\":\"autocomplete-1643896472928-0\",\"requireValidOption\":false,\"data\":\"airport_codes\",\"values\":[]}]' WHERE `categories`.`id` = 8;
 
