633cc29c563958f0b6531c38a80ae05eb15f2f2d
kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>
2022-03-08 12:11:42 +0500

    add station's feild in seeder

--- database/seeds/sql_dump/categories.sql
+++ database/seeds/sql_dump/categories.sql
@@ -23,6 +23,7 @@ UPDATE `categories` SET `second_label_of_time` = 'Arrival Time' WHERE `categorie
 
 --  Train
 UPDATE `categories` SET `set_end_date_of_service` = '1' WHERE `categories`.`id` = 7;
+UPDATE `categories` SET `feilds` = '[{\"type\":\"autocomplete\",\"label\":\"Departure Station\",\"className\":\"form-control\",\"name\":\"autocomplete-1646723271296-0\",\"requireValidOption\":false,\"data\":\"stations\",\"values\":[]},{\"type\":\"autocomplete\",\"label\":\"Arrival Station\",\"className\":\"form-control\",\"name\":\"autocomplete-1646723272930-0\",\"requireValidOption\":false,\"data\":\"stations\",\"values\":[]}]' WHERE `categories`.`id` = 7;
 
 -- Flights
 UPDATE `categories` SET `feilds` = '[{\"type\":\"autocomplete\",\"label\":\"Departure Airport\",\"className\":\"form-control\",\"name\":\"autocomplete-1643896471930-0\",\"requireValidOption\":false,\"data\":\"airport_codes\",\"values\":[]},{\"type\":\"autocomplete\",\"label\":\"Arrival Airport\",\"className\":\"form-control\",\"name\":\"autocomplete-1643896472928-0\",\"requireValidOption\":false,\"data\":\"airport_codes\",\"values\":[]}]' WHERE `categories`.`id` = 8;
