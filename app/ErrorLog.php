be4c7fb91ed941cfc906d7b482059496c756238b
kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>
2022-03-21 19:27:20 +0500

    error log work

---
+++ app/ErrorLog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ErrorLog extends Model
+{
+    protected $fillable = [
+        'file',
+        'line',
+        'message'
+    ];
+}
