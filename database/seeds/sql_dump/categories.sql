2a6378b16c93b447744c6789bb1f42b10ea1b90b
kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>
2022-03-28 19:53:58 +0500

    Tour Category feild move into Tour Product feilds

--- database/seeds/sql_dump/categories.sql
+++ database/seeds/sql_dump/categories.sql
@@ -8,7 +8,6 @@ UPDATE `categories` SET `label_of_time` = 'Time of Service' WHERE `categories`.`
 UPDATE `categories` SET `feilds` = '[{\"type\":\"text\",\"label\":\"Room Type\",\"className\":\"form-control\",\"name\":\"text-1643891680692-0\",\"subtype\":\"text\"}]' WHERE `categories`.`id` = 2;
 
 --  Tour
-UPDATE `categories` SET `feilds` = '[{\"type\":\"text\",\"label\":\"Meeting Point\",\"className\":\"form-control\",\"name\":\"text-1646218903106-0\",\"subtype\":\"text\"},{\"type\":\"text\",\"label\":\"Contact\",\"className\":\"form-control\",\"name\":\"text-1646218842639-0\",\"subtype\":\"text\"},{\"type\":\"number\",\"label\":\"Telephone&nbsp;\",\"className\":\"form-control\",\"name\":\"number-1646218867708-0\"},{\"type\":\"textarea\",\"label\":\"Address\",\"className\":\"form-control\",\"name\":\"textarea-1646218878256-0\",\"subtype\":\"textarea\"}]' WHERE `categories`.`id` = 3;
 UPDATE `categories` SET `set_end_date_of_service` = '1' WHERE `categories`.`id` = 3;
 
 --  Cruise
