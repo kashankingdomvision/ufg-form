fc346c035cf983e2dd0cd49b2c48c21b144a54d2
kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>
2022-04-02 11:43:56 +0500

    set time of service in flight, transfer, train

--- database/seeds/sql_dump/categories.sql
+++ database/seeds/sql_dump/categories.sql
@@ -1,8 +1,12 @@
 
 -- Transfer
 UPDATE `categories` SET `feilds` = '[{\"type\":\"autocomplete\",\"label\":\"Pick up Location\",\"className\":\"form-control\",\"name\":\"autocomplete-1643896315062-0\",\"requireValidOption\":false,\"data\":\"harbours\",\"values\":[]},{\"type\":\"autocomplete\",\"label\":\"Drop off Location\",\"className\":\"form-control\",\"name\":\"autocomplete-1643896316045-0\",\"requireValidOption\":false,\"data\":\"harbours\",\"values\":[]}]' WHERE `categories`.`id` = 1;
+
 UPDATE `categories` SET `show_tf` = '1' WHERE `categories`.`id` = 1;
-UPDATE `categories` SET `label_of_time` = 'Time of Service' WHERE `categories`.`id` = 1;
+UPDATE `categories` SET `label_of_time` = 'Departure Time' WHERE `categories`.`id` = 1;
+
+UPDATE `categories` SET `second_tf` = '1' WHERE `categories`.`id` = 1;
+UPDATE `categories` SET `second_label_of_time` = 'Arrival Time' WHERE `categories`.`id` = 1;
 
 -- Accommodation
 UPDATE `categories` SET `feilds` = '[{\"type\":\"text\",\"label\":\"Room Type\",\"className\":\"form-control\",\"name\":\"text-1643891680692-0\",\"subtype\":\"text\"}]' WHERE `categories`.`id` = 2;
@@ -30,6 +34,13 @@ UPDATE `categories` SET `second_label_of_time` = 'Arrival Time' WHERE `categorie
 UPDATE `categories` SET `set_end_date_of_service` = '1' WHERE `categories`.`id` = 7;
 UPDATE `categories` SET `feilds` = '[{\"type\":\"autocomplete\",\"label\":\"Departure Station\",\"className\":\"form-control\",\"name\":\"autocomplete-1646723271296-0\",\"requireValidOption\":false,\"data\":\"stations\",\"values\":[]},{\"type\":\"autocomplete\",\"label\":\"Arrival Station\",\"className\":\"form-control\",\"name\":\"autocomplete-1646723272930-0\",\"requireValidOption\":false,\"data\":\"stations\",\"values\":[]}]' WHERE `categories`.`id` = 7;
 
+UPDATE `categories` SET `show_tf` = '1' WHERE `categories`.`id` = 7;
+UPDATE `categories` SET `label_of_time` = 'Departure Time' WHERE `categories`.`id` = 7;
+
+UPDATE `categories` SET `second_tf` = '1' WHERE `categories`.`id` = 7;
+UPDATE `categories` SET `second_label_of_time` = 'Arrival Time' WHERE `categories`.`id` = 7;
+
+
 -- Flights
 UPDATE `categories` SET `feilds` = '[{\"type\":\"autocomplete\",\"label\":\"Departure Airport\",\"className\":\"form-control\",\"name\":\"autocomplete-1643896471930-0\",\"requireValidOption\":false,\"data\":\"airport_codes\",\"values\":[]},{\"type\":\"autocomplete\",\"label\":\"Arrival Airport\",\"className\":\"form-control\",\"name\":\"autocomplete-1643896472928-0\",\"requireValidOption\":false,\"data\":\"airport_codes\",\"values\":[]}]' WHERE `categories`.`id` = 8;
 
@@ -41,3 +52,6 @@ UPDATE `categories` SET `second_label_of_time` = 'Arrival Time' WHERE `categorie
 
 --  Misc.
 UPDATE `categories` SET `feilds` = '[{\"type\":\"text\",\"label\":\"Misc Details\",\"className\":\"form-control\",\"name\":\"text-1643892011594-0\",\"subtype\":\"text\"}]' WHERE `categories`.`id` = 9;
+
+-- UPDATE `categories` SET `show_tf` = '1' WHERE `categories`.`id` = 1;
+-- UPDATE `categories` SET `label_of_time` = 'Time of Service' WHERE `categories`.`id` = 1;
